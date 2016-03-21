09fc198bb6641c194e3676e296bc641e3afc3d37
emchoko <emilfrogy@gmail.com>
2016-03-21 23:45:29 +0200

    Moved the drawing route function in new class

--- app/src/main/java/emillozev/sofiatraffic/UI/MainActivity.java
+++ app/src/main/java/emillozev/sofiatraffic/UI/MainActivity.java
@@ -7,9 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.NavigationView;
@@ -20,7 +18,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,21 +31,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import emillozev.sofiatraffic.R;
+import emillozev.sofiatraffic.UI.DirectionsAndNavigation.DrawRoute;
 import emillozev.sofiatraffic.UI.Fragments.ImportFragment;
 
 public class MainActivity extends AppCompatActivity
@@ -58,6 +45,7 @@ public class MainActivity extends AppCompatActivity
     private GoogleMap mMap;
     private final int REQUEST_CODE_PERMISSION = 0;
     private ArrayList<LatLng> markerPoints;
+    public DrawRoute mRoute;
 
 
 
@@ -89,6 +77,7 @@ public class MainActivity extends AppCompatActivity
 
         mMapFragment.getMapAsync(this);
         mMap = mMapFragment.getMap();
+        mRoute = new DrawRoute(mMap);
     }
 
     @Override
@@ -171,6 +160,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //DrawRoute mRoute = new DrawRoute(mMap);
         mMap = googleMap;
         markerPoints = new ArrayList<LatLng>();
         Button btnDraw = (Button)findViewById(R.id.btn_draw);
@@ -179,7 +169,6 @@ public class MainActivity extends AppCompatActivity
         mMap.setTrafficEnabled(true);
         // Add a marker in Sofia and move the camera
         LatLng sofia = new LatLng(42.697626, 23.322284);
-        LatLng plovdiv = new LatLng(42.1438, 24.7496);
         float zoomLevel = (float) 11.00; //This goes up to 21
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sofia, zoomLevel));
 
@@ -215,7 +204,7 @@ public class MainActivity extends AppCompatActivity
 
                 // Already 10 locations with 8 waypoints and 1 start location and 1 end location.
                 // Upto 8 waypoints are allowed in a query for non-business users
-                if(markerPoints.size()>=10){
+                if (markerPoints.size() >= 10) {
                     return;
                 }
 
@@ -233,11 +222,11 @@ public class MainActivity extends AppCompatActivity
                  * for the end location, the color of marker is RED and
                  * for the rest of markers, the color is AZURE
                  */
-                if(markerPoints.size()==1){
+                if (markerPoints.size() == 1) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                }else if(markerPoints.size()==2){
+                } else if (markerPoints.size() == 2) {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                }else{
+                } else {
                     options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
                 }
 
@@ -245,7 +234,6 @@ public class MainActivity extends AppCompatActivity
                 mMap.addMarker(options);
             }
         });
-
 
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
 
@@ -269,12 +257,10 @@ public class MainActivity extends AppCompatActivity
                     LatLng dest = markerPoints.get(1);
 
                     // Getting URL to the Google Directions API
-                    String url = getDirectionsUrl(origin, dest);
+                    String url = mRoute.getDirectionsUrl(origin, dest);
 
-                    DownloadTask downloadTask = new DownloadTask();
+                    mRoute.downloadTask(url);
 
-                    // Start downloading json data from Google Directions API
-                    downloadTask.execute(url);
                 }
             }
         });
@@ -349,166 +335,4 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try{
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb  = new StringBuffer();
-
-            String line = "";
-            while( ( line = br.readLine())  != null){
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        }catch(Exception e){
-            Log.d("Download Exception: ", e.toString());
-        }finally{
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-    // Fetches data from url passed
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        // Downloading data in non-ui thread
-        @Override
-        protected String doInBackground(String... url) {
-
-            // For storing data from web service
-
-            String data = "";
-
-            try{
-                // Fetching the data from web service
-                data = downloadUrl(url[0]);
-            }catch(Exception e){
-                Log.d("Background Task",e.toString());
-            }
-            return data;
-        }
-
-        // Executes in UI thread, after the execution of
-        // doInBackground()
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            // Invokes the thread for parsing the JSON data
-            parserTask.execute(result);
-        }
-    }
-
-    /** A class to parse the Google Places in JSON format */
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try{
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                // Starts parsing data
-                routes = parser.parse(jObject);
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        // Executes in UI thread, after the parsing process
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-
-            ArrayList<LatLng> points = null;
-            PolylineOptions lineOptions = null;
-
-            // Traversing through all the routes
-            for(int i=0;i<result.size();i++){
-                points = new ArrayList<LatLng>();
-                lineOptions = new PolylineOptions();
-
-                // Fetching i-th route
-                List<HashMap<String, String>> path = result.get(i);
-
-                // Fetching all the points in i-th route
-                for(int j=0;j<path.size();j++){
-                    HashMap<String,String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                // Adding all the points in the route to LineOptions
-                lineOptions.addAll(points);
-                lineOptions.width(11);
-                lineOptions.color(Color.BLUE);
-            }
-
-            // Drawing polyline in the Google Map for the i-th route
-            mMap.addPolyline(lineOptions);
-        }
-    }
-
-
-    private String getDirectionsUrl(LatLng origin,LatLng dest){
-
-        // Origin of route
-        String str_origin = "origin="+origin.latitude+","+origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination="+dest.latitude+","+dest.longitude;
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-
-        // Waypoints
-        String waypoints = "";
-        for(int i=2;i<markerPoints.size();i++){
-            LatLng point  = (LatLng) markerPoints.get(i);
-            if(i==2)
-                waypoints = "waypoints=";
-            waypoints += point.latitude + "," + point.longitude + "|";
-        }
-
-        // Building the parameters to the web service
-        String parameters = str_origin+"&"+str_dest+"&"+sensor+"&"+waypoints;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
-
-        return url;
-    }
 }
