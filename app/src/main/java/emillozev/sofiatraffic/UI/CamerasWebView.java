4d7b7412748cc3c3fe496d72e1314eaa5fd9a249
emchoko <emilfrogy@gmail.com>
2016-05-15 17:27:35 +0300

    Cameras in Sofia

--- app/src/main/java/emillozev/sofiatraffic/UI/CamerasWebView.java
+++ app/src/main/java/emillozev/sofiatraffic/UI/CamerasWebView.java
@@ -1,5 +1,7 @@
 package emillozev.sofiatraffic.UI;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
 
+import emillozev.sofiatraffic.Fragments.CamerasFragment;
 import emillozev.sofiatraffic.R;
 
 public class CamerasWebView extends AppCompatActivity {
@@ -19,13 +22,10 @@ public class CamerasWebView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
-
-        //setContentView(webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        //webView.getSettings().set
-        webView.loadUrl("http://docs.google.com/gview?embedded=true&url=" +"http://kat.mvr.bg/NR/rdonlyres/FC9B850F-9001-4659-97F1-9A4E48FD71C7/0/askpd_tkontrol.pdf");
-        //setContentView(webview);
+        WebView mWebView=new WebView(CamerasWebView.this);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://docs.google.com/gview?embedded=true&url="+"http://kat.mvr.bg/NR/rdonlyres/FC9B850F-9001-4659-97F1-9A4E48FD71C7/0/askpd_tkontrol.pdf");
+        setContentView(mWebView);
     }
 
 }
