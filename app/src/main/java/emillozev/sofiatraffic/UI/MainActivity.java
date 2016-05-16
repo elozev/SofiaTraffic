1007eed88c99e23b684d9e796094b4e3928fb6ae
emchoko <emilfrogy@gmail.com>
2016-05-16 19:15:46 +0300

    removed one of the two permission requests

--- app/src/main/java/emillozev/sofiatraffic/UI/MainActivity.java
+++ app/src/main/java/emillozev/sofiatraffic/UI/MainActivity.java
@@ -25,6 +25,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -42,7 +43,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -72,7 +76,7 @@ import emillozev.sofiatraffic.Fragments.ImportFragment;
 import emillozev.sofiatraffic.Fragments.NavigationFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener, GoogleApiClient.OnConnectionFailedListener {
 
     public SupportMapFragment mMapFragment;
     private GoogleMap mMap;
@@ -86,6 +90,8 @@ public class MainActivity extends AppCompatActivity
     private boolean isSearchButtonOnMap = true;
     private Button mSpeedButton;
     private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private SignInButton signInButton;
 
 
 
@@ -113,10 +119,11 @@ public class MainActivity extends AppCompatActivity
                 .build();
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setScopes(gso.getScopeArray());
 
@@ -124,7 +131,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                //startActivityForResult(signInIntent, RC_SIGN_IN);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
         //GOOGLE SIGN IN
@@ -238,6 +245,34 @@ public class MainActivity extends AppCompatActivity
         }
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("GOOGLE", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            Toast.makeText(MainActivity.this, "LOGIN " + acct.getDisplayName(), Toast.LENGTH_SHORT).show();
+            signInButton.setVisibility(View.GONE);
+
+           // updateUI(true);
+        } else {
+            Toast.makeText(MainActivity.this, "LOGIN " + "FAIL", Toast.LENGTH_SHORT).show();
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
+    }
+
 
     public void addFragmentToDisplay(Class fragmentClass){
         final FragmentManager fm = getFragmentManager();
@@ -400,28 +435,6 @@ public class MainActivity extends AppCompatActivity
 
         checkIfLocationTurned();
         isInternetConnected();
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            this.mMap.setMyLocationEnabled(true);
-        } else {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                    showMessageOKCancel("You need to allow access to Location Services",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                            REQUEST_CODE_PERMISSION);
-                                }
-                            });
-                    return;
-                }
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        REQUEST_CODE_PERMISSION);
-                return;
-            }
-        }
 
         if (isGetDirectionsClicked == true && addToMapPolyline != null) {
             mMap.addPolyline(addToMapPolyline);
@@ -617,6 +630,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onProviderDisabled(String provider) {
         Log.d("Latitude", "status");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     private class GeoCoderIsShit extends AsyncTask<String, Integer, List<LatLng>> {
